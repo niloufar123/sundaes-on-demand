f81629cd65bffdec5484ea521a87a04e8f897171
niloufar_alinezhad <niloufar_alinezhad@fdeshragh.com>
2022-05-30 05:25:17 +0430

    checkbox fireEvend to enable/disable button

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,16 +1,17 @@
 import { Form,Button,Container  } from "react-bootstrap";
 
-import React from "react";
+import React, { useState } from "react";
 
 function SummaryForm() {
+  const [enable,setEnable]=useState(false);
   return (
     <Container>
 
       <Form>
-       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out"/>
+       <Form.Group className="mb-3" controlId="formBasicCheckbox"  onChange={()=>setEnable(!enable)}>
+          <Form.Check type="checkbox" label="terms and conditionst" />
         </Form.Group>
-        <Button variant="primary" type="submit" name='submit'>
+        <Button variant="primary" type="submit" name='submit' disabled={!enable}>
           Submit
         </Button>
       </Form>
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import SummaryForm from "../SummaryForm";
 
-test("check default checkBox value",()=>{
+test("check default checkBox value and click action",()=>{
     render(<SummaryForm/>);
 
-    const checkBox=screen.getByRole('checkbox',{name:/Check me out/i});
+    const checkBox=screen.getByRole('checkbox',{name:/terms and conditions/i});
     // expect(checkBox).not.toBeChecked
-    expect(checkBox).toHaveProperty('checked',false)
+    expect(checkBox).toHaveProperty('checked',false);
+    fireEvent.click(checkBox);
+    const button=screen.getByRole('button',{name:/confirm order/i});
+    expect(button).toBeEnabled();
+    fireEvent.click(checkBox);
+    expect(button).toBeDisabled();
+
+
 })
