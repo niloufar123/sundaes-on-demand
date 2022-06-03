431c5abf733c3d89f74f55a556de7633074aebc0
niloufar_alinezhad <niloufar_alinezhad@fdeshragh.com>
2022-06-04 00:56:03 +0430

    popup label hover with user Event

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,20 +1,45 @@
-import { Form,Button,Container  } from "react-bootstrap";
+import { Form, Button, Container, Popover, Overlay } from "react-bootstrap";
 
 import React, { useState } from "react";
 
 function SummaryForm() {
-  const [enable,setEnable]=useState(false);
+  const [enable, setEnable] = useState(false);
+  const [showPopUp, setShowPopUp] = useState(0);
   return (
     <Container>
-
       <Form>
-       <Form.Group className="mb-3" controlId="formBasicCheckbox"  onChange={()=>setEnable(!enable)}>
-          <Form.Check type="checkbox" label="terms and conditionst" />
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicCheckbox"
+          onChange={() => setEnable(!enable)}
+        >
+          <div className="form-check">
+            <input
+              type="checkbox"
+              id="default-checkbox"
+              className="form-check-input"
+            />
+            <label
+              title=""
+              htmlFor="default-checkbox"
+              className="form-check-label"
+              onMouseOver={() => setShowPopUp(1)}
+              onMouseLeave={() => setShowPopUp(0)}
+            >
+              terms and conditionst
+            </label>
+          </div>
         </Form.Group>
-        <Button variant="primary" type="submit" name='submit' disabled={!enable}>
-        confirm order
+        <Button
+          variant="primary"
+          type="submit"
+          name="submit"
+          disabled={!enable}
+        >
+          confirm order
         </Button>
       </Form>
+      <div style={{ opacity: `${showPopUp}` }}>Popover testing</div>
     </Container>
   );
 }
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,17 +1,38 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import SummaryForm from "../SummaryForm";
+import userEvent from '@testing-library/user-event'
 
 test("check default checkBox value and click action",()=>{
     render(<SummaryForm/>);
 
     const checkBox=screen.getByRole('checkbox',{name:/terms and conditions/i});
     // expect(checkBox).not.toBeChecked
+    // fireEvent
     expect(checkBox).toHaveProperty('checked',false);
-    fireEvent.click(checkBox);
+    userEvent.click(checkBox);
     const button=screen.getByRole('button',{name:/confirm order/i});
     expect(button).toBeEnabled();
-    fireEvent.click(checkBox);
+    userEvent.click(checkBox);
     expect(button).toBeDisabled();
 
 
+});
+
+test("popover responds to hover",()=>{
+    render(<SummaryForm />);
+    //popover starts out hidden
+     const label=screen.getByLabelText("terms and conditionst");
+     const overlay=screen.getByText("Popover testing");
+     
+     expect(overlay).not.toBeVisible();
+
+
+    //popover appears when mouseover of checkbox label
+
+    userEvent.hover(label);
+    expect(overlay).toBeVisible();
+
+    //popover disappears when mouse out
+    userEvent.unhover(label);
+    expect(overlay).not.toBeVisible();
 })
